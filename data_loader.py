0dc16bdf270c7248c81cc0d050a6d3af48551724
slycer <slycer@slycerBig.slycer.org>
2020-06-15 11:08:23 +0200

    load geo data and filter data

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,53 @@
 import pandas as pd
+import geopandas as gpd
 from datetime import date
+import json
 
-# TODO: maby change to not transform data to day by day data
-# but use a view instead in bokeh which filteres for specific data
+data_file_covid_ecdc = './data/U99TR3NJ.csv'
+data_file_geo_europe = './data/europe.geojson'
+
 def load_data():
-    df = load_data_ecdc()
+    geo_data, europe_iso_list = load_data_geo()
+    covid_raw = load_data_ecdc()
+    # filter for european data and date 2020
+    covid_data = filter_data_ecdc(covid_raw, europe_iso_list)
+
+    return covid_data, geo_data
+
+def load_data_ecdc():
+    df = pd.read_csv(data_file_covid_ecdc)
+    df['date'] = df.apply(lambda row: date(year = row['year'], month = row['month'], day = row['day']), axis = 1)
+    return df.drop(columns=['dateRep', 'day', 'month', 'year'])
+
+def load_data_geo():
+    gdf = gpd.read_file(data_file_geo_europe)
+    gdf = gdf[gdf['ISO2'] != 'IL'] # remove Israel
+    europe_iso_list = list(gdf.ISO3)
+    return gdf.to_json(), europe_iso_list
+
+def filter_data_ecdc(df, iso_list):
+    df_filtered = df[df.countryterritoryCode.isin(iso_list)]
+
+    frst_date = date.fromisoformat('2020-01-01')
+    df_filtered = df_filtered[df_filtered.date >= frst_date]
+
+    return df_filtered.copy().reset_index(drop = True)
+
+# old unused functions from here on-->
+
+# load without geopandas
+def load_data_geo_old():
+    data = None
+    with open(data_file_geo_europe) as f:
+        data = json.load(f)
+    return json.dumps(data)
+
+# transrorm the covid data into a dictionary by date
+# it seems better to filter in the bokeh view
+def transform_to_date_dict(df):
     data = {}
 
     dates = sorted(df.date.unique())
     for date in dates:
-        data[date] = df[df.date == date].copy().reset_index()
+        data[date] = df[df.date == date].copy().reset_index(drop = True)
     return data
-
-
-def load_data_ecdc():
-    df = pd.read_csv('./data/U99TR3NJ.csv')
-    df['date'] = df.apply(lambda row: date(year = row['year'], month = row['month'], day = row['day']), axis = 1)
-    return df.drop(columns=['dateRep', 'day', 'month', 'year'])
