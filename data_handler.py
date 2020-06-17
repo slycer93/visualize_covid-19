b231ab32e4f7200c644a654103211707413947d5
slycer <slycer@slycerBig.slycer.org>
2020-06-17 11:01:33 +0200

    rename and minor changes

--- data_handler.py
+++ data_handler.py
@@ -11,27 +11,36 @@ def load_data():
     covid_raw = load_data_ecdc()
     # filter for european data and date 2020
     covid_data = filter_data_ecdc(covid_raw, europe_iso_list)
+    # create dates list
+    dates = create_dates_list(covid_data)
 
-    return covid_data, geo_data
+    return covid_data, geo_data, dates
 
 def load_data_ecdc():
     df = pd.read_csv(data_file_covid_ecdc)
+    df = df.rename(columns={"countryterritoryCode": "ISO3",})
     df['date'] = df.apply(lambda row: date(year = row['year'], month = row['month'], day = row['day']), axis = 1)
-    return df.drop(columns=['dateRep', 'day', 'month', 'year'])
+    return df.drop(columns=['dateRep', 'day', 'month', 'year', 'geoId', 'continentExp'])
 
 def load_data_geo():
     gdf = gpd.read_file(data_file_geo_europe)
     gdf = gdf[gdf['ISO2'] != 'IL'] # remove Israel
     europe_iso_list = list(gdf.ISO3)
-    return gdf.to_json(), europe_iso_list
+    return gdf, europe_iso_list
 
 def filter_data_ecdc(df, iso_list):
-    df_filtered = df[df.countryterritoryCode.isin(iso_list)]
+    df_filtered = df[df.ISO3.isin(iso_list)]
 
     frst_date = date.fromisoformat('2020-03-01')
     df_filtered = df_filtered[df_filtered.date >= frst_date]
 
     return df_filtered.copy().reset_index(drop = True)
+
+def create_dates_list(df):
+    dates = list(sorted(df.date.unique()))
+    days = [i for i in range(0,len(dates))]
+    dates = dict(zip(days, dates))
+    return dates
 
 # old unused functions from here on-->
 
